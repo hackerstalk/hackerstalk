6f80618cc5a8a727ed3a6bd1a704846ae0984982
Jeongbong Seo <jngbng@gmail.com>
2017-01-29 21:46:41 +0900

    Reindent

--- main.go
+++ main.go
@@ -1,79 +1,79 @@
 package main
 
 import (
-	"log"
-	"net/http"
-	"os"
+  "log"
+  "net/http"
+  "os"
 
-	"github.com/gin-contrib/gzip"
-	"gopkg.in/gin-gonic/gin.v1"
+  "github.com/gin-contrib/gzip"
+  "gopkg.in/gin-gonic/gin.v1"
 )
 
 func main() {
-	var err error
+  var err error
 
-	// 환경변수에서 DB, PORT정보 가져옴
-	dbUrl := os.Getenv("DATABASE_URL")
-	if dbUrl == "" {
-		dbUrl = "postgresql://localhost/ht?sslmode=disable"
-	}
+  // 환경변수에서 DB, PORT정보 가져옴
+  dbUrl := os.Getenv("DATABASE_URL")
+  if dbUrl == "" {
+    dbUrl = "postgresql://localhost/ht?sslmode=disable"
+  }
 
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-	}
+  port := os.Getenv("PORT")
+  if port == "" {
+    port = "8080"
+  }
 
-	// DB 연결
-	err = Connect(dbUrl)
-	if err != nil {
-		log.Fatalln(err)
-	}
+  // DB 연결
+  err = Connect(dbUrl)
+  if err != nil {
+    log.Fatalln(err)
+  }
 
-	// GIN Routing
-	router := gin.New()
-	router.Use(gin.Recovery())
-	router.Use(gzip.Gzip(gzip.DefaultCompression))
+  // GIN Routing
+  router := gin.New()
+  router.Use(gin.Recovery())
+  router.Use(gzip.Gzip(gzip.DefaultCompression))
 
-	if gin.IsDebugging() {
-		router.Use(gin.Logger())
-	}
+  if gin.IsDebugging() {
+    router.Use(gin.Logger())
+  }
 
-	router.LoadHTMLGlob("templates/*.html")
+  router.LoadHTMLGlob("templates/*.html")
 
-	router.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.html", nil)
-	})
+  router.GET("/", func(c *gin.Context) {
+    c.HTML(http.StatusOK, "index.html", nil)
+  })
 
-	router.POST("/api/login", func(c *gin.Context) {
-		name := c.DefaultPostForm("name", "bbirec")
-		githubId := c.DefaultPostForm("githubId", "bbirec")
+  router.POST("/api/login", func(c *gin.Context) {
+    name := c.DefaultPostForm("name", "bbirec")
+    githubId := c.DefaultPostForm("githubId", "bbirec")
 
-		err := NewUser(name, githubId)
-		if err != nil {
-			c.JSON(500, gin.H{
-				"status": "FAIL",
-				"msg":    err.Error(),
-			})
-			return
-		}
+    err := NewUser(name, githubId)
+    if err != nil {
+      c.JSON(500, gin.H{
+        "status": "FAIL",
+        "msg":    err.Error(),
+      })
+      return
+    }
 
-		c.JSON(200, gin.H{
-			"status": "OK",
-		})
-	})
+    c.JSON(200, gin.H{
+      "status": "OK",
+    })
+  })
 
-	router.GET("/api/link", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"status": "OK",
-		})
-	})
+  router.GET("/api/link", func(c *gin.Context) {
+    c.JSON(200, gin.H{
+      "status": "OK",
+    })
+  })
 
-	router.POST("/api/link", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"status": "OK",
-		})
-	})
+  router.POST("/api/link", func(c *gin.Context) {
+    c.JSON(200, gin.H{
+      "status": "OK",
+    })
+  })
 
-	router.Static("/static", "static")
-	router.Run(":" + port)
+  router.Static("/static", "static")
+  router.Run(":" + port)
 }
