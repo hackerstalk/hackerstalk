8c173cef1c67278527bc2aad18e90460ad3b71f7
Jeongbong Seo <jngbng@gmail.com>
2017-02-05 22:54:51 +0900

    Reindent sql file

--- sql/schema.sql
+++ sql/schema.sql
@@ -2,24 +2,24 @@
 
 create table users
 (
-	id serial primary key,
-	name varchar(255) not null,
-	github_id varchar(255) unique,
+  id serial primary key,
+  name varchar(255) not null,
+  github_id varchar(255) unique,
 
-	edited_time timestamp,
-	created_time timestamp default CURRENT_TIMESTAMP
+  edited_time timestamp,
+  created_time timestamp default CURRENT_TIMESTAMP
 );
 
 create table links
 (
-	id serial primary key,
+  id serial primary key,
 
-	url text,
-	tags character varying[],
-	comment text,
-	user_id int references users(id) not null,
+  url text,
+  tags character varying[],
+  comment text,
+  user_id int references users(id) not null,
 
-	edited_time timestamp,
-	created_time timestamp default CURRENT_TIMESTAMP
+  edited_time timestamp,
+  created_time timestamp default CURRENT_TIMESTAMP
 );
 
