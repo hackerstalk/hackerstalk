67c5df542361bccfc82cccde21cb351feaa807fb
Heehong Moon(문희홍) <bbirec@gmail.com>
2017-02-10 18:22:41 +0900

    Mail chimp 폼 추가.

--- client/app.jsx
+++ client/app.jsx
@@ -6,7 +6,7 @@ import { getCookie, setCookie } from './util.js';
 import { LinkList } from './listLink.jsx';
 
 
-import { Button } from 'elemental';
+import { Button, InputGroup, FormIconField, FormInput } from 'elemental';
 
 const App = React.createClass({
   displayName: 'App',
@@ -41,6 +41,24 @@ const App = React.createClass({
         </div>
         <div className="content">
           {this.props.children}
+
+          <div className="lead">해커스톡 메일링 리스트</div>
+          <div id="mc_embed_signup">
+          <form action="//dabangapp.us8.list-manage.com/subscribe/post?u=126e16ff4f89785ae63578adc&amp;id=020aedb0ef" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" className="validate" target="_blank" noValidate>
+            <div id="mc_embed_signup_scroll">
+            <InputGroup contiguous>
+              <InputGroup.Section grow>
+                <FormIconField width="one-half" iconPosition="left" iconColor="default" iconKey="mail">
+                  <FormInput placeholder="Email address" name="email" name="EMAIL"  id="mce-EMAIL" required />
+                </FormIconField>
+              </InputGroup.Section>
+              <InputGroup.Section>
+                <Button type="primary" submit={true} name="b_126e16ff4f89785ae63578adc_020aedb0ef">등록</Button>
+              </InputGroup.Section>
+            </InputGroup>
+            </div>
+          </form>
+          </div>
         </div>
       </div>
     );
