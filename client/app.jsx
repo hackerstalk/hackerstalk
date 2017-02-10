12d924d9c9cabfb64ce029dd628cdf0b64d97779
Heehong Moon(문희홍) <bbirec@gmail.com>
2017-02-10 18:30:30 +0900

    문구 추가.

--- client/app.jsx
+++ client/app.jsx
@@ -43,13 +43,14 @@ const App = React.createClass({
           {this.props.children}
 
           <div className="lead">해커스톡 메일링 리스트</div>
+          <p>매주 개발 관련된 뉴스를 이메일로 보내드립니다.</p>
           <div id="mc_embed_signup">
           <form action="//dabangapp.us8.list-manage.com/subscribe/post?u=126e16ff4f89785ae63578adc&amp;id=020aedb0ef" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" className="validate" target="_blank" noValidate>
             <div id="mc_embed_signup_scroll">
             <InputGroup contiguous>
               <InputGroup.Section grow>
                 <FormIconField width="one-half" iconPosition="left" iconColor="default" iconKey="mail">
-                  <FormInput placeholder="Email address" name="email" name="EMAIL"  id="mce-EMAIL" required />
+                  <FormInput placeholder="이메일 주소" name="email" name="EMAIL"  id="mce-EMAIL" required />
                 </FormIconField>
               </InputGroup.Section>
               <InputGroup.Section>
