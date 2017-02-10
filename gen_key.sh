3a48a4a80ebcfb63849c13f94e80fe44abe4d65e
Heehong Moon(문희홍) <bbirec@gmail.com>
2017-02-10 21:41:15 +0900

    다른 방식으로 변경.

--- gen_key.sh
+++ gen_key.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-STATIC_KEY=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+STATIC_KEY=$(date | md5sum)
 echo "Generating Static Key : $STATIC_KEY"
 echo "export STATIC_KEY=$STATIC_KEY" > ~/.profile.d/static.sh
