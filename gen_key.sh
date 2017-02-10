5bb9351a22641d8f1f7c5c01c57be7a7fa235d1e
Heehong Moon(문희홍) <bbirec@gmail.com>
2017-02-10 21:50:40 +0900

    설명 추가.

--- gen_key.sh
+++ gen_key.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+# Build time에 unique한 static key를 생성한다.
+# 환경변수 STATIC_KEY으로 사용되며, static resource는 build마다 unique한 path로 사용된다.
+
 STATIC_KEY=$(date | md5sum)
 echo "Generating Static Key : $STATIC_KEY"
 echo "export STATIC_KEY=$STATIC_KEY" > $PWD/.profile.d/static.sh
